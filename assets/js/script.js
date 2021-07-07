f6a185f423d39edd9d190466c5b08e8034a16840
Érick de Oliveira Teixeira <38773400+ErickOliveiraT@users.noreply.github.com>
2021-07-07 03:16:44 -0300

    Animações spin, scale e hinge

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,7 @@ $(function () {
     var $yeti = $('#yeti');
     var $transitionList = $('#transitionList');
     var flag = false;
+    var animating_html, animating_css;
 
     $('#transitioner').click(function () {
         MotionUI.animateIn($yeti, $transitionList.val());
@@ -17,5 +18,33 @@ $(function () {
             MotionUI.animateIn('#sobre-img', 'slide-in-left');
             flag = true;
         }        
+    });
+
+    $('#img_html').hover(function () {
+        MotionUI.animateIn('#img_html', 'scale-in-down');
+    });
+
+    $('#img_css').hover(function () {   
+        MotionUI.animateIn('#img_css', 'scale-in-down');
+    });
+
+    $('#img_js').hover(function () {
+        MotionUI.animateIn('#img_js', 'spin-in');
+    });
+
+    $('#img_react').hover(function () {   
+        MotionUI.animateIn('#img_react', 'spin-in');
+    });
+
+    $('#hobbie1').hover(function() {
+        MotionUI.animateIn('#hobbie1', 'hinge-in-from-middle-y');
+    });
+
+    $('#hobbie2').hover(function() {
+        MotionUI.animateIn('#hobbie2', 'hinge-in-from-top');
+    });
+
+    $('#hobbie3').hover(function() {
+        MotionUI.animateIn('#hobbie3', 'hinge-in-from-bottom');
     });
 });
